095c291f91443e2a11e6d051305f99fb627057d0
AdamSelki <adamselki@yahoo.com>
2022-11-11 15:12:04 -0800

    Schema screenshot

--- app/api/stories_routes.py
+++ app/api/stories_routes.py
@@ -1,4 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from app.models import Story
 
 stories_routes = Blueprint('stories', __name__)
 
+@stories_routes.route('/')
+def get_stories():
+    data = Story.query.all()
+    return {'data': [story.to_dict() for story in data]}
+
+
+@stories_routes.route('/', methods=['POST'])
+def post_story():
+    data = request.json
+    story = Story(title=data['title'],
+                body=data['body'],
+                user_id=data['user_id']
+                )
