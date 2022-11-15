5a5aae914ac81ffa55809a0b446c83750852fe4d
Jessie Baron <jessiebaron.political@gmail.com>
2022-11-15 11:34:56 -0500

    added comment reducer

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import stories from './stories'
+import commentReducer from './comment'
 
 const rootReducer = combineReducers({
   session,
-  stories
+  stories,
+  comment: commentReducer
 });
 
 
