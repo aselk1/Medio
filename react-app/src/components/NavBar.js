9405befb228da13c932c40a504ed96db73dcbd69
Jessie Baron <jessiebaron.political@gmail.com>
2022-11-14 20:02:33 -0500

    css

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,34 +2,28 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import "./Navigation.css"
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+    <nav className='navigation-container'>
+      <img className="logo" src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="logo" />
+      <ul className='navigation'>
+          <NavLink className='navigation-text' to='/' exact={true} activeClassName='active'>
             Home
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
+          <NavLink className='navigation-text' to='/login' exact={true} activeClassName='active'>
+            <LogoutButton />
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <NavLink className='navigation-text' to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
           </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
+          <NavLink className='navigation-text' to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
+        <button id='get__started'><NavLink id='get-started-text' to='/sign-up' exact={true} activeClassName='active'>
+            Get Started
+        </NavLink></button>
       </ul>
     </nav>
   );
