d77dbe0d533a9e94cbb039fcbf7c6c2d96b4cfe2
AdamSelki <adamselki@yahoo.com>
2022-11-14 19:30:30 -0800

    Fixed fetch with csrf.js file

--- react-app/src/store/stories.js
+++ react-app/src/store/stories.js
@@ -1,37 +1,39 @@
-const POST_STORY = 'stories/POST_STORY';
-const GET_STORIES = 'stories/GET_STORIES';
+import { csrfFetch } from "./csrf";
 
+const POST_STORY = "stories/POST_STORY";
+const GET_STORIES = "stories/GET_STORIES";
 
 const postStory = (story) => ({
-    type: POST_STORY,
-    payload: story
-})
+  type: POST_STORY,
+  payload: story,
+});
 
 const getStories = (stories) => ({
   type: GET_STORIES,
-  payload: stories
+  payload: stories,
 });
 
 export const fetchAllStories = () => async (dispatch) => {
-  const response = await fetch("api/stories");
+  const response = await fetch("/api/stories");
   if (response.ok) {
-    const stories = await response.json()
-    dispatch(getStories(stories.data))
-    return response
+    const stories = await response.json();
+    dispatch(getStories(stories.data));
+    return response;
   }
-}
+};
 
 export const fetchPostStory = (story) => async (dispatch) => {
+  console.log("working");
   const { title, body } = story;
   const formData = new FormData();
   formData.append("title", title);
   formData.append("body", body);
-  const response = await fetch("api/stories", {
+  const response = await csrfFetch("/api/stories", {
     method: "POST",
     headers: {
-      "Content-Type": "multipart/form-data"
+      "Content-Type": "multipart/form-data",
     },
-    body: formData
+    body: formData,
   });
   if (response.ok) {
     const story = await response.json();
@@ -40,13 +42,13 @@ export const fetchPostStory = (story) => async (dispatch) => {
   }
 };
 
-const initialState = {}
+const initialState = {};
 
 export default function reducer(state = initialState, action) {
-  let newState
+  let newState;
   switch (action.type) {
     case GET_STORIES:
-      newState = action.payload
+      newState = action.payload;
       return newState;
     case POST_STORY:
       newState = Object.assign({}, state);
