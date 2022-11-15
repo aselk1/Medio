e0d632ec0f28fe072a34363ffd8c2bf05b3b1f5b
Erin Dar <erindar@Erins-iMac.local>
2022-11-15 12:07:49 -0500

    Fixes to followers route

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
 import stories from './stories'
+import follower from './follower'
 
 const rootReducer = combineReducers({
   session,
-  stories
+  stories,
+  follower
 });
 
 
