877305f3315404e4440528194bb44e0a7d95db5f
Erin Dar <erindar@Erins-iMac.local>
2022-11-15 16:48:29 -0500

    Working on individual story cards

--- react-app/src/components/Feed/StoryFeed/StoryCard.js
+++ react-app/src/components/Feed/StoryFeed/StoryCard.js
@@ -3,7 +3,9 @@ import { NavLink } from "react-router-dom"
 export default function StoryCard({ story }) {
     return (
         <div className="story-card">
-            <div className="story-author"></div>
+            <div className="story-author">
+                <h1>{story}</h1>
+            </div>
         </div>
     )
 }
