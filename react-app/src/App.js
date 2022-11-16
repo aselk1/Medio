19799bfbea58797edbda716d6bc244e9cdc734d7
Jessie Baron <jessiebaron.political@gmail.com>
2022-11-15 19:13:05 -0500

    consolidate

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
+import SideBar from './components/SideBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import UsersList from './components/UsersList';
 import User from './components/User';
@@ -12,6 +13,7 @@ import LogoutButton from './components/auth/LogoutButton';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
+  const [showMenu, setShowMenu] = useState(false)
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -35,26 +37,7 @@ function App() {
           <SignUpForm />
         </Route>
         <Route path='/home' exact={true}>
-          <div className='sideBar'>
-              <img className='home-logo' src="https://miro.medium.com/max/1400/1*psYl0y9DUzZWtHzFJLIvTw.png" alt="logo" />
-              <div>
-                <i id="icon2" class="fa-solid fa-house-chimney"></i>
-              </div>
-              <div>
-                <i id="icon3" class="fa-regular fa-file-lines"></i>
-              </div>
-              <div>
-                <i id="icon4" class="fa-regular fa-pen-to-square"></i>
-              </div>
-              <div>
-                <i id="icon5" class="fa-solid fa-ellipsis"></i>
-              </div>
-              <img />
-              <img />
-              <img />
-              <img />
-              <img />
-          </div>
+          <SideBar />
         </Route>
         <ProtectedRoute path='/users' exact={true} >
           <UsersList/>
