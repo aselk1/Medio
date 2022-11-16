a82e1e5f45e3c5fb1f9156c93b17a8f9568c617f
Erin Dar <erindar@Erins-iMac.local>
2022-11-16 11:31:31 -0500

    fixed splash and sidebar routes

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch, NavLink } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
@@ -10,11 +10,13 @@ import { authenticate } from './store/session';
 import StoryForm from './components/story/StoryForm';
 import Stories from "./components/story/Stories";
 import StoryDetails from './components/story/StoryDetails';
-import MainPage from './components/MainPage';
+import SplashPage from './components/SplashPage';
 import FollowFeed from './components/Feed/FollowFeed/FollowFeed';
+import SideBar from './components/SideBar';
 
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(state => state.session)
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
@@ -31,8 +33,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' exact={true}>
-          <MainPage />
+        <Route exact path='/'>
+          {user ? <Redirect to='/home' /> : <SplashPage />}
+        </Route>
+        <Route exact path='/home'>
+          <SideBar />
         </Route>
         <Route path="/new-story" exact={true}>
           <StoryForm />
