9db55aa434fe780a682c7afdc16e4d6474e84ff1
AdamSelki <adamselki@yahoo.com>
2022-11-17 13:35:38 -0800

    render hard refresh fix

--- react-app/src/components/SignupFormModal.js/SignupForm.js
+++ react-app/src/components/SignupFormModal.js/SignupForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Redirect } from "react-router-dom";
 import { signUp } from "../../store/session";
+import './SignupForm.css'
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
@@ -43,51 +44,45 @@ const SignUpForm = () => {
   }
 
   return (
-    <form onSubmit={onSignUp}>
+    <div className="width700">
+    <form onSubmit={onSignUp} className="flexCol">
       <div>
         {errors.map((error, ind) => (
           <div key={ind}>{error}</div>
         ))}
       </div>
-      <div>
-        <label>User Name</label>
         <input
           type="text"
           name="username"
+          placeholder="User Name"
           onChange={updateUsername}
           value={username}
         ></input>
-      </div>
-      <div>
-        <label>Email</label>
         <input
           type="text"
           name="email"
+          placeholder="Email"
           onChange={updateEmail}
           value={email}
         ></input>
-      </div>
-      <div>
-        <label>Password</label>
         <input
           type="password"
           name="password"
+          placeholder="Password"
           onChange={updatePassword}
           value={password}
         ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
         <input
           type="password"
           name="repeat_password"
+          placeholder="Repeat Password"
           onChange={updateRepeatPassword}
           value={repeatPassword}
           required={true}
         ></input>
-      </div>
       <button type="submit">Sign Up</button>
     </form>
+    </div>
   );
 };
 
