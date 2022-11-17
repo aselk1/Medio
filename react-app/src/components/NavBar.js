97aec499c879c27d38fd8dc51def9fae19a17fee
Erin Dar <erindar@Erins-iMac.local>
2022-11-16 20:03:12 -0500

    styling splash page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import "./Navigation.css"
+import "./NavBar.css"
 
 const NavBar = () => {
 
@@ -22,24 +23,39 @@ const NavBar = () => {
 
 
   return (
-    <div className={navBar ? 'navigation-container active' : 'navigation-container'}>
+    <div className={navBar ? 'navigation-container nav-active' : 'navigation-container'}>
       <div className='nav-bar-container'>
-        <div className='nav-bar'>
-          <img className="logo" src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="logo" />
-          <ul className='navigation'>
-            <NavLink className='navigation-text' to='/' exact={true} activeClassName='active'>
-              Home
-            </NavLink>
-            <NavLink className='navigation-text' to='/login' exact={true} activeClassName='active'>
-              Sign In
-            </NavLink>
-            <NavLink className='navigation-text' to='/sign-up' exact={true} activeClassName='active'>
-              Sign Up
-            </NavLink>
-            <button id='get__started'><NavLink id='get-started-text' to='/sign-up' exact={true} activeClassName='active'>
-              Get Started
-            </NavLink></button>
-          </ul>
+        <div className='nav bar'>
+          <div className='navigation-items'>
+            <div className='medio-logo'>
+              <NavLink to='/' exact={true}>
+                <img className="logo" src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png" alt="logo" />
+              </NavLink>
+            </div>
+            <div className='spacer' />
+            <div className='nav-links'>
+              <span className='nav-modal-container'>
+                <div className='nav-modal'>
+                  <p className='modal-login'>
+                    <span>
+                      <NavLink className='navigation-text' to='/login' exact={true} activeClassName='active'>
+                        Sign In
+                      </NavLink>
+                    </span>
+                  </p>
+                </div>
+              </span>
+              <div className='get-started'>
+                <span>
+                  <NavLink id='get-started-text' to='/sign-up' exact={true} activeClassName='active'>
+                    <button id='get__started'>
+                      Get Started
+                    </button>
+                  </NavLink>
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
