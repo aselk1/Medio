3be3226cd574d0f51f25a410142b1655304b05c3
Jessie Baron <jessiebaron.political@gmail.com>
2022-11-18 09:08:48 -0500

    fix

--- react-app/src/components/story/StoryDetails.js
+++ react-app/src/components/story/StoryDetails.js
@@ -180,7 +180,7 @@ const StoryDetails = () => {
                           ></img><h2>{user?.username}</h2>
                         </div>
                         <div className="textarea-comments"><CommentForm /></div>
-                        {story.Comments?.map((comment) => (
+                        {story?.Comments?.map((comment) => (
                           <div>
                             <img
                               src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png"
@@ -218,7 +218,7 @@ const StoryDetails = () => {
                       <div className="sb-spacer"></div>
                       <NavLink to={`/users/${story.user_id}`} className='profile-link'>
                         <h2 className="profile-author-name">
-                          <span className="user">{story.User.username}</span>
+                          <span className="user">{story?.User?.username}</span>
                         </h2>
                       </NavLink>
                       <div className="follow-button-holder">
